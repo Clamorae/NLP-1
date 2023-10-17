ba9f5481fc095af009be190cc61e00e23e37388d
Clamorae <trombiniquentin@gmail.com>
2023-10-17 12:31:04 +0800

    created list with label and word

--- test_example.py
+++ test_example.py
@@ -1,8 +1,27 @@
-﻿with open('./test-submit.txt','r') as f:
+﻿import torch
+import torch.nn as nn
+
+with open('./NLP/NLP-1/train.txt','r') as f:
     lines = f.readlines()
 
-with open('./example.txt','w') as f:
+#create an array with the dataset
+sentences = []
+label = []
+
+with open('./NLP/NLP-1/example.txt','w') as f:
+    current_word = []
+    current_label = []
     for line in lines:
-        if line=='\n': f.write(line)
+        if line=='\n': 
+            f.write(line)
+            sentences.append(current_word)
+            label.append(current_label)
+            current_word = []
+            current_label = []
         else:
-            f.write(line.replace('\n','\tO\n'))
+            separate = line.split('\t')
+            current_word.append(separate[0])
+            current_label.append(separate[1].split('\n')[0])
+            #f.write(line.replace('\n','\tO\n'))
+print(sentences)
+print(label)
