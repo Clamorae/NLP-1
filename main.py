7a149f987547b3cc9bbb3d59e1a447f34a28a185
Clamorae <trombiniquentin@gmail.com>
2023-10-28 12:27:33 +0800

    maybe it's finally a great datapreprocess

--- main.py
+++ main.py
@@ -2,22 +2,25 @@
 import string
 
 import torch.nn as nn
+import torch.nn.functional as F
 
-from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, TensorDataset
 from gensim.models import Word2Vec
 from torch.autograd import Variable
 
 # --------------------------------- CONSTANT --------------------------------- #
 PATH = "./NLP/NLP-1/"
-WORD_EMB_DIM = 128
-EPOCHS = 20
+BATCH_SIZE = 64
 
+word_emb_dim = 128
+epochs = 20
 learning_rate = 0.01
-emb_size = 128
+windows = 5
 layer_dim = 4
 hidden_dim = 4
+dropout = 0.1
 
-# ------------------------------- DATA CREATION ------------------------------ #
+# ------------------------------- DATA LOADING ------------------------------ #
 class Loader():
     def __init__(self, path) -> None:
         self.item = []
@@ -25,13 +28,20 @@ class Loader():
 
         with open(path,'r') as f:
             lines = f.readlines()
+        sentence = []
+        label = []
         for line in lines:
             if line!='\n': 
                 separate = line.split('\t')
                 if separate[0] not in string.punctuation:
-                    self.item.append(separate[0])
-                    self.target.append(separate[1].split('\n')[0])
-    
+                    sentence.append(separate[0])
+                    label.append(separate[1].split('\n')[0])
+            else:
+                self.item.append(sentence)
+                self.target.append(label)
+                sentence = []
+                label = []
+        
     def getItem(self):
         return self.item
     
@@ -45,33 +55,76 @@ class Loader():
         self.target = label
 
 train_loader = Loader(PATH+"train.txt")
+val_loader = Loader(PATH+"dev.txt")
 
-train_embedding_model = Word2Vec(sentences=train_loader.getItem(), vector_size=128, window=5, min_count=1, workers=8, sg = 1)
-train_embedding = train_embedding_model.wv[list(train_embedding_model.wv.key_to_index.keys())].tolist()
-train_embedding = [[0.5] * WORD_EMB_DIM] + [[0.] * WORD_EMB_DIM] + train_embedding
+# ---------------------------- DATA EMBEDDING ---------------------------- #
 
-vocab = ["<PAD>","<UNK>"] + list(set(train_loader.getLabel()))
+embedding_model = Word2Vec(sentences=train_loader.getItem(),vector_size=word_emb_dim,window=windows,min_count=1,workers=8,sg=1)
+word_embedding = [[0.5] * word_emb_dim, [0.] * word_emb_dim]
+
+vocab = list(embedding_model.wv.index_to_key)
+for word in vocab:
+    word_embedding.append(embedding_model.wv[word].tolist())
+
+vocab = ['<PAD>', '<UNK>'] + vocab
 vocab_size = len(vocab)
-vocab_tensor = []
-for i in range(vocab_size):
-    vocab_tensor.append(torch.tensor([0.0]*i + [1.0] + [0.0]*(vocab_size-i-1)))
 
+index2word = vocab
+word2index = dict((j,i) for i,j in enumerate(vocab))
 
-train_data = [train_embedding,train_loader.getLabel()]
+index2target = ['<PAD>','O','B-tvshow','I-tvshow','B-geo-loc','I-geo-loc','B-company','I-company','B-person','I-person','B-movie','I-movie','B-facility','I-facility','B-sportsteam','I-sportsteam','B-musicartist','I-musicartist','B-product','I-product','B-other','I-other']
+nb_class = len(index2target)
+
+target2index = dict((j,i) for i,j in enumerate(index2target))
+
+def encoder(data,word2index,word_embedding):
+    dataset = []
+    for line in data:
+        current_sentence =[]
+        for word in line:
+            if word in word2index:
+                index = word2index.get(word)
+            else:
+                index = word2index.get("<UNK>")
+            current_sentence.append(word_embedding[index])
+        dataset.append(current_sentence)
+    return(dataset)
+
+# ---------------------------------- PADDING --------------------------------- #
+
+max_size = max(len(seq) for seq in train_loader.getItem())
+
+def addPadding(data, max_size, windows):
+    padded_data = []
+    for line in data:
+        diff = max_size - len(line)
+        padded_line = ['<PAD>'] * windows + line + ['<PAD>'] * (windows + diff)
+        padded_data.append(padded_line)
+    return padded_data
+
+train_loader.setItem(addPadding(train_loader.getItem(),max_size,windows))
+val_loader.setItem(addPadding(val_loader.getItem(),max_size,windows))
+
+# -------------------------------- DATALOADER -------------------------------- #
+
+train_int = [[target2index[target] for target in words] for words in train_loader.getItem()]
+train_words_embedded = encoder(train_loader.getItem(), word2index, word_embedding)
+train_inputs = torch.Tensor(train_words_embedded).float()
+train_targets = torch.Tensor(train_int).long()
+train_dataset = TensorDataset(train_inputs, train_targets)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 # ------------------------------- RNN CREATION ------------------------------- #
 
 class RNN(nn.Module):
-    def __init__(self,hidden_dim,layer_dim,input_dim,output_dim):
+    def __init__(self,hidden_dim,layer_dim,input_dim,output_dim,dropout_rate):
         super(RNN,self).__init__()
-        # Number of hidden dimensions
+
         self.hidden_dim = hidden_dim
-        
-        # Number of hidden layers
         self.layer_dim = layer_dim
         
         # RNN
-        self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim, batch_first=True, nonlinearity='relu')
+        self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim, batch_first=True, nonlinearity='relu',dropout=dropout_rate)
         
         # Readout layer
         self.fc = nn.Linear(hidden_dim, output_dim)
@@ -82,29 +135,28 @@ class RNN(nn.Module):
         h0 = Variable(torch.zeros(self.layer_dim, self.hidden_dim))
         x,hn = self.rnn(x,h0)
         x = self.fc(x)
+        x = F.softmax(x, dim=1)
         return x
 
-rnn = RNN(hidden_dim,layer_dim,emb_size,vocab_size)        
+rnn = RNN(hidden_dim,layer_dim,word_emb_dim,vocab_size,dropout)        
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
 
 # --------------------------------- TRAINING --------------------------------- #
 
-for epoch in range(EPOCHS):
-    true = 0
-    o = 0
-    for i in range(len(train_data[0])):
+for epoch in range(epochs):
+    sum_loss = 0
+    correct = 0
+    total = 0
+
+    for sentence, label in train_loader:
+
         optimizer.zero_grad()   
-        outputs = rnn(torch.tensor(train_data[0][i])[None, ...])
-        lab = (vocab_tensor[vocab.index(train_data[1][i])])
-        loss = criterion(outputs,lab.view(-1,23))
+        outputs = rnn(sentence)
+        loss = criterion(outputs,label)
         loss.backward()
         optimizer.step()
-        if vocab[torch.argmax(outputs)] == train_data[1][i]:
-            true+=1
-        if train_data[1][i] == "O":
-            o+=1
         
-    print(f"{true}/{i}, O = {o}")
-    print(f"EPOCH : {epoch}, accuracy = {true/i*100}%")
+        sum_loss+=loss.item()
+        
 
